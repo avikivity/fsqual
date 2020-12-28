b4d66945a8219814a6d0241b9ad6e8c5c4e101ea
Avi Kivity <avi@scylladb.com>
2020-12-28 14:05:33 +0200

    Make pretrunate a parameter
    Simplifies adding more conditions to the test matrix.

--- fsqual.cc
+++ fsqual.cc
@@ -41,7 +41,10 @@ with_ctxsw_counting(Counter& counter, Func&& func) {
     return func();
 }
 
-void test_concurrent_append(io_context_t ioctx, int fd, unsigned iodepth, size_t bufsize, std::string mode) {
+void test_concurrent_append(io_context_t ioctx, int fd, unsigned iodepth, size_t bufsize, bool pretruncate) {
+    if (pretruncate) {
+        ftruncate(fd, off_t(1) << 30);
+    }
     auto nr = 10000;
     auto ctxsw = 0;
     auto buf = aligned_alloc(4096, bufsize);
@@ -71,6 +74,7 @@ void test_concurrent_append(io_context_t ioctx, int fd, unsigned iodepth, size_t
     }
     auto rate = float(ctxsw) / nr;
     auto verdict = rate < 0.1 ? "GOOD" : "BAD";
+    auto mode = std::string(pretruncate ? "size-changing" : "size-unchanging");
     std::cout << "context switch per appending io (mode " << mode << ", iodepth " << iodepth << "): " << rate
           << " (" << verdict << ")\n";
     auto ptr = mmap(nullptr, nr * 4096, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
@@ -79,11 +83,6 @@ void test_concurrent_append(io_context_t ioctx, int fd, unsigned iodepth, size_t
     if (std::any_of(incore.begin(), incore.end(), [] (uint8_t m) { return m & 1; })) {
         std::cout << "Seen data in page cache (BAD)\n";
     }
-}
-
-void test_concurrent_append_size_unchanging(io_context_t ioctx, int fd, unsigned iodepth, size_t bufsize, std::string mode) {
-    ftruncate(fd, off_t(1) << 30);
-    test_concurrent_append(ioctx, fd, iodepth, bufsize, mode);
 }
 
 void run_test(std::function<void (io_context_t ioctx, int fd)> func) {
@@ -129,10 +128,10 @@ int main(int ac, char** av) {
     std::cout << "memory DMA alignment:    " << info.memory_alignment << "\n";
     std::cout << "disk DMA alignment:      " << info.disk_alignment << "\n";
 
-    run_test([] (io_context_t ioctx, int fd) { test_concurrent_append(ioctx, fd, 1, 4096, "size-changing"); });
-    run_test([] (io_context_t ioctx, int fd) { test_concurrent_append(ioctx, fd, 3, 4096, "size-changing"); });
-    run_test([] (io_context_t ioctx, int fd) { test_concurrent_append_size_unchanging(ioctx, fd, 3, 4096, "size-unchanging"); });
-    run_test([] (io_context_t ioctx, int fd) { test_concurrent_append_size_unchanging(ioctx, fd, 7, 4096, "size-unchanging"); });
-    run_test([=] (io_context_t ioctx, int fd) { test_concurrent_append_size_unchanging(ioctx, fd, 1, info.disk_alignment, "size-unchanging, sector aligned"); });
+    run_test([] (io_context_t ioctx, int fd) { test_concurrent_append(ioctx, fd, 1, 4096, false); });
+    run_test([] (io_context_t ioctx, int fd) { test_concurrent_append(ioctx, fd, 3, 4096, false); });
+    run_test([] (io_context_t ioctx, int fd) { test_concurrent_append(ioctx, fd, 3, 4096, true); });
+    run_test([] (io_context_t ioctx, int fd) { test_concurrent_append(ioctx, fd, 7, 4096, true); });
+    run_test([=] (io_context_t ioctx, int fd) { test_concurrent_append(ioctx, fd, 1, info.disk_alignment, true); });
     return 0;
 }
